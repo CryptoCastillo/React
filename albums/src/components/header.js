5d9cfc044b4dc8c5641ea0019b560cc3636a6140
Crypto Castillo <cryptocastillollc@gmail.com>
2018-02-23 22:54:46 -0800

    bleh falling asleep
    zZz

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 // Make a component
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>Albums{props.headerText}</Text>
     </View>
   );
 };
@@ -22,7 +22,9 @@ const styles = {
     paddingTop: 15,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   },
   textStyle: {
     fontSize: 20
@@ -31,3 +33,7 @@ const styles = {
 
 // Make the component available to other parts of the app
 export default Header;
+
+
+
+//start at S7 L29
