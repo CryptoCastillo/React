f76eea9d1f04c6041ec32584b43decf4576aace7
Crypto Castillo <cryptocastillollc@gmail.com>
2018-02-24 18:38:54 -0800

    Update
    Installed Axios to make API call for albums.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
+  }
+
   render() {
     return (
       <View>
@@ -10,4 +16,5 @@ class AlbumList extends Component {
     );
   }
 }
+
 export default AlbumList;
